2a99fd4e40cd67d259618830f84bedb8a55ff3a9
xiaojunheng <710286171@qq.com>
2016-01-18 18:07:18 +0800

    passport

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,153 +1,132 @@
-var express = require('express'),
+const express = require('express'),
     home = express.Router(),
     mongoose = require('mongoose'),
     path = require('path'),
     formidable = require('formidable'),
     fs = require('fs'),
-    passport = require('passport'),
-    User = mongoose.model('User');
+    User = mongoose.model('User'),
+    passport=require('./../../config/passport');
 
 module.exports = function(app) {
     app.use('/', home)
 }
 
-home.route('/').get(function(req, res, next) {
-    res.render('index', {
-        title: 'Generator-Express MVC'
+home.route('/').get((req, res, next) => {
+    res.render('login', {
+        title: '登录',
+        message:null
     })
 })
 
-
-home.route('/register').get(function(req, res, next) {
+home.route('/register').get((req, res, next) => {
         res.render('register', {
-            title: '注册'
+            title: '注册',
+            message: null
         })
     })
-    .post(function(req, res, next) {
-        var headImg = null;
-        var form = new formidable.IncomingForm(); //创建上传表单
-        form.encoding = 'utf-8'; //设置编辑
-        form.uploadDir = path.normalize('./././public/upload/'); //设置上传目录
-        form.keepExtensions = true; //保留后缀
-        form.maxFieldsSize = 2 * 1024 * 1024; //文件大小
+    .post((req, res, next) => {
+        var headImg = 'default.jpg'
+        var form = new formidable.IncomingForm() //创建上传表单
+        form.encoding = 'utf-8' //设置编辑
+        form.uploadDir = path.normalize('./././public/upload/headimg') //设置上传目录
+        form.keepExtensions = true //保留后缀
+        form.maxFieldsSize = 2 * 1024 * 1024 //文件大小
         form.parse(req, function(err, fields, files) { //fields 表单的其它参数
-            console.log('fields.username =' + fields.username)
-            console.log('============================== into formidable method ================================')
-            if (err) {
-                res.locals.error = err;
-                res.render('index', {
-                    msg: err
-                });
-                return;
-            }
-            var extName = ''; //后缀名
-            switch (files.img.type) {
-                case 'image/pjpeg':
-                    extName = 'jpg';
-                    break;
-                case 'image/jpeg':
-                    extName = 'jpg';
-                    break;
-                case 'image/png':
-                    extName = 'png';
-                    break;
-                case 'image/x-png':
-                    extName = 'png';
-                    break;
-            }
-
-            if (extName.length == 0) {
-                res.locals.error = '只支持png和jpg格式图片';
-                res.render('/', {
-                    msg: '只支持png和jpg格式图片'
-                });
-                return;
-            }
-
-            var avatarName = Math.random() + '.' + extName;
-            var newPath = form.uploadDir + avatarName;
-
-            console.log("newPath:" + newPath);
-            fs.renameSync(files.img.path, newPath); //重命名
-            console.log("=========================== 上传图片成功 =================================")
-
+            //res.send(JSON.stringify(files))
             User.findOne({
                 username: fields.username
             }, function(err, doc) { // 同理 /login 路径的处理方式
                 if (err) {
                     res.send(500)
-                    console.log(err)
                 } else if (doc) {
-                    res.send(fields.username + '已存在');
+                    res.render("register", {
+                        message: fields.username + '已存在'
+                    })
+                    return
                 } else {
+                    //图片上传
+                    if (files.img.size != 0) {
+                        if (err) {
+                            res.render('register', {
+                                message: err
+                            })
+                            return
+                        }
+                        var extName = '' //后缀名
+                        switch (files.img.type) {
+                            case 'image/pjpeg':
+                                extName = 'jpg'
+                                break
+                            case 'image/jpeg':
+                                extName = 'jpg'
+                                break
+                            case 'image/png':
+                                extName = 'png'
+                                break
+                            case 'image/x-png':
+                                extName = 'png'
+                                break
+                        }
+
+                        if (extName.length == 0) {
+                            res.render('register', {
+                                message: '头像只支持png和jpg格式图片'
+                            })
+                            return
+                        }
+
+                        headImg = Math.random() + '.' + extName
+                        var newPath = form.uploadDir + headImg
+
+                        fs.renameSync(files.img.path, newPath) //重命名
+
+                    }
 
                     User.register(new User({ // 创建一组user对象置入model
-                            username: fields.username,
-                            password: fields.password,
-                            isVip: 1,
-                            headImg: newPath,
-                            phone: fields.phone,
-                            registerTime: new Date(),
-                            lastLoginTime: null,
-                            ip: fields.ip,
-                            city: fields.city,
-                            region: fields.region,
-                            country: fields.country,
-                            loc: fields.loc,
-                            org: fields.org
-                        }), fields.password, function(err, user) {
-                            if (err) {
-                                return res.render("register", {
-                                    info: "Sorry. That username already exists. Try again."
-                                });
-                            }
-                            res.redirect('/login')
-                                /*passport.authenticate('local')(req, res, function() {
-                                    req.session.save(function(err) {
-                                        if (err) {
-                                            return next(err);
-                                        }
-                                        res.redirect('/index');
-                                    });
-                                });*/
-                        })
-                        /*User.create({ // 创建一组user对象置入model
-                            username: fields.username,
-                            password: fields.password,
-                            isVip: 1,
-                            headImg: newPath,
-                            phone: fields.phone,
-                            registerTime: new Date(),
-                            lastLoginTime: null,
-                            ip: fields.ip,
-                            city: fields.city,
-                            region: fields.region,
-                            country: fields.country,
-                            loc: fields.loc,
-                            org: fields.org
-                        }, function(err, doc) {
-                            if (err) {
-                                res.send(500);
-                                console.log(err);
-                            } else {
-                                res.redirect('/login')
-                            }
-                        })*/
+                        username: fields.username,
+                        password: fields.password,
+                        isVip: 1,
+                        headImg: 'upload\\headimg' + headImg, //考虑将路径配置成全局变量
+                        phone: fields.phone,
+                        registerTime: new Date(),
+                        lastLoginTime: null,
+                        ip: fields.ip,
+                        city: fields.city,
+                        region: fields.region,
+                        country: fields.country,
+                        loc: fields.loc,
+                        org: fields.org
+                    }), fields.password, (err, user) => {
+                        if (err) {
+                            res.render("register", {
+                                message: "Sorry. That username already exists. Try again."
+                            })
+                            return
+                        }
+                        res.redirect('/login')
+                    })
                 }
             })
 
         })
     })
 
-home.route('/login').get(function(req, res, next) {
+home.route('/login').get((req, res, next) => {
     res.render('login', {
-        title: '登录'
+        title: '登录',
+        message: null
     })
-}).post(passport.authenticate('local'), function(req, res) {
-    req.session.save(function(err) {
-        if (err) {
-            return next(err)
-        }
-        res.redirect('/')
+}).post((req, res, next) => {
+    passport.login(req, res, next)
+})
+
+home.route('/logout').get((req, res, next) => {
+    passport.logout(req, res, next)
+})
+
+home.route('/index').get((req, res, next) => {
+    res.render('index', {
+        title: '首页',
+        user: req.user
     })
 })
---
+++ config/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+
+//用户登录验证
+exports.login = function(req, res, next) {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            next(err)
+            return
+        }
+        if (!user) {
+            res.render('login', {
+                title: '登录失败',
+                message: info.message
+            })
+            return
+        }
+        if (user.isVip == 1) {
+            res.render('login', {
+                title: '登录失败',
+                message: '抱歉,你的帐号还未激活.'
+            })
+            return
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                next(err)
+                return
+            }
+            res.redirect('/index') //跳转首页
+            return
+        })
+    })(req, res, next);
+
+}
+
+exports.logout = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        req.logOut()
+    }
+    res.render('login', {
+        title: '登录',
+        message: null
+    })
+}
+
+//将req.isAuthenticated()封装成中间件
+exports.isAuthenticated = function(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/login', {
+        title: '登录',
+        message: null
+    });
+}
